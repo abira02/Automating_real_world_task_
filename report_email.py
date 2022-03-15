f8d74ca09fb9100ce5ce6a77acf93f92be6a1e2d
abira02 <99014671+abira02@users.noreply.github.com>
2022-03-15 19:07:43 +0530

    Update report_email.py

--- report_email.py
+++ report_email.py
@@ -2,4 +2,22 @@
 import os
 import requests
 import datetime
-path = 'supplier-data/descriptions'
+import reports
+def pdf_body(path):
+  pdf_content = []
+  for files in os.listdir(path):
+     with open (path+'/'+files) as f:
+        item_name = f.readline().rstrip()
+        item_weight = f.readline().rstrip()
+     pdf_content.append([item_name,item_weight])
+  pdf_body = ''
+  for item in pdf_content:
+      pdf_body += 'name:'+item[0]+'<br />'+'weight:'+item[1]+'<br />'+'<br />'
+  return pdf_body
+if __name__ == '__main__':
+  path = 'supplier-data/descriptions'
+  paragraph = pdf_body(path)
+  current_date = datetime.date.today().strftime("%B %d, %Y")
+  attachment = '/tmp/processed.pdf'
+  title = 'Process Updated on '+ str(current_date)
+  pdf = reports.generate_report(attachment, title, paragraph)
